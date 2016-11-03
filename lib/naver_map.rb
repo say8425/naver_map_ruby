b8229e5d0ff35091297d75d5b0fd6111796dbadc
Penguin <say8425@gmail.com>
2016-11-03 14:26:52 +0900

    define puts

--- lib/naver_map.rb
+++ lib/naver_map.rb
@@ -6,5 +6,9 @@ module NaverMap
       @client_id = client_id
       @client_secret = client_secret
     end
+
+    def to_s
+      "Client ID: #{@client_id}, Client Secret: #{@client_secret}"
+    end
   end
 end
