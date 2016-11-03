b2a2089aa345aaaba84a1630929b371385a90cf5
Jitae Kim <originerd@gmail.com>
2016-11-03 19:13:19 +0900

    Refactoring rescue clauses in method

--- lib/naver_map.rb
+++ lib/naver_map.rb
@@ -11,27 +11,23 @@ class NaverMap
   end
 
   def address_to_coordinates(address)
-    begin
-      url = 'https://openapi.naver.com/v1/map/geocode'
-      response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
-                                'X-Naver-Client-Secret': @client_secret)
-      result = JSON.parse(response.body, symbolize_names: true)[:result]
-      result[:items].map do |element| element[:point] end
-    rescue RestClient::ExceptionWithResponse => err
-      err.response.body
-    end
+    url = 'https://openapi.naver.com/v1/map/geocode'
+    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
+                              'X-Naver-Client-Secret': @client_secret)
+    result = JSON.parse(response.body, symbolize_names: true)[:result]
+    result[:items].map do |element| element[:point] end
+  rescue RestClient::ExceptionWithResponse => err
+    err.response.body
   end
 
 
   def query(address)
-    begin
-      url = 'https://openapi.naver.com/v1/map/geocode'
-      response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
-                                'X-Naver-Client-Secret': @client_secret)
-      JSON.parse(response.body, symbolize_names: true)[:result]
-    rescue RestClient::ExceptionWithResponse => err
-      err.response.body
-    end
+    url = 'https://openapi.naver.com/v1/map/geocode'
+    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
+                              'X-Naver-Client-Secret': @client_secret)
+    JSON.parse(response.body, symbolize_names: true)[:result]
+  rescue RestClient::ExceptionWithResponse => err
+    err.response.body
   end
 
   def to_s
