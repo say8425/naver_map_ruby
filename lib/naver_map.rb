6fd0965e5d51211f403b27e1a2c8b36a21a2ed35
Penguin <say8425@gmail.com>
2016-11-03 14:27:18 +0900

    define client id and client secret getter

--- lib/naver_map.rb
+++ lib/naver_map.rb
@@ -10,5 +10,13 @@ module NaverMap
     def to_s
       "Client ID: #{@client_id}, Client Secret: #{@client_secret}"
     end
+
+    def client_id
+      @client_id
+    end
+
+    def client_secret
+      @client_secret
+    end
   end
 end
