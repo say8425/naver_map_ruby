6c9062c6f742619b300b275ca3df8ddbfe5a21e7
Penguin <say8425@gmail.com>
2016-11-03 15:56:36 +0900

    define coordinates to address
    if you want shot with header then just put it in within params

--- lib/naver_map.rb
+++ lib/naver_map.rb
@@ -1,4 +1,5 @@
 require "naver_map/version"
+require 'rest-client'
 
 module NaverMap
   class NaverMap
@@ -7,6 +8,11 @@ module NaverMap
     def initialize(client_id, client_secret)
       @client_id = client_id
       @client_secret = client_secret
+    end
+
+    def coordinates_to_address(address)
+      url = 'https://openapi.naver.com/v1/map/geocode'
+      RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id, 'X-Naver-Client-Secret': @client_secret).body
     end
 
     def to_s
