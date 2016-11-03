79412e74ad479cfb5957ffc676c7bc79b2810898
Jitae Kim <originerd@gmail.com>
2016-11-03 19:14:07 +0900

    Refactoring code styles
    - use accessor instead of class level instance variable
    - use single block

--- lib/naver_map.rb
+++ lib/naver_map.rb
@@ -12,10 +12,10 @@ class NaverMap
 
   def address_to_coordinates(address)
     url = 'https://openapi.naver.com/v1/map/geocode'
-    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
-                              'X-Naver-Client-Secret': @client_secret)
+    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': client_id,
+                              'X-Naver-Client-Secret': client_secret)
     result = JSON.parse(response.body, symbolize_names: true)[:result]
-    result[:items].map do |element| element[:point] end
+    result[:items].map { |element| element[:point] }
   rescue RestClient::ExceptionWithResponse => err
     err.response.body
   end
@@ -23,14 +23,14 @@ class NaverMap
 
   def query(address)
     url = 'https://openapi.naver.com/v1/map/geocode'
-    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': @client_id,
-                              'X-Naver-Client-Secret': @client_secret)
+    response = RestClient.get(url, params: { query: address }, 'X-Naver-Client-Id': client_id,
+                              'X-Naver-Client-Secret': client_secret)
     JSON.parse(response.body, symbolize_names: true)[:result]
   rescue RestClient::ExceptionWithResponse => err
     err.response.body
   end
 
   def to_s
-    "Client ID: #{@client_id}, Client Secret: #{@client_secret}"
+    "Client ID: #{client_id}, Client Secret: #{client_secret}"
   end
 end
