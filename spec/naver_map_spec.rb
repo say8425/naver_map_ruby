6969a1b9d6cf1e798dd2ae27b154b1dc4908f6a0
Dohyung Ahn <emaren84@gmail.com>
2016-11-08 14:19:10 +0900

    Implement coordinates to address function

--- spec/naver_map_spec.rb
+++ spec/naver_map_spec.rb
@@ -11,6 +11,12 @@ describe NaverMap do
         nm.address_to_coordinates('서울특별시 성동구 왕십리로 88 노벨빌딩 4층').should == [{:x=>127.0452879, :y=>37.5454765}]
       end
     end
+
+    context 'given an coordinates' do
+      it 'return address' do
+        nm.coordinates_to_address('127.1141382', '37.3599968').should == ["경기도 성남시 분당구 정자동 257-1"]
+      end
+    end
   end
 
 end
